afd8ebd726966e6455b1a8688cb65b0d49054fbb
philip <philster36@hotmail.co.uk>
2023-02-17 20:56:59 +0000

    home page stuff

--- src/componenets/layout/Navbar.tsx
+++ src/componenets/layout/Navbar.tsx
@@ -1,6 +1,5 @@
 import { NavLink } from "react-router-dom";
 import './Navbar.css'
-import Typewriter from "../features/Typewriter";
 import React, { Dispatch, SetStateAction, useEffect, useState } from 'react'
 
 interface Page {
@@ -27,7 +26,6 @@ export default function Navbar(props: NavbarProps): JSX.Element {
         } else {
             setTheme('light');
         }
-        
     }
     
     useEffect(() => {
@@ -40,7 +38,7 @@ export default function Navbar(props: NavbarProps): JSX.Element {
                 <div id="theme-button" onClick={toggleTheme}>SWAP</div>
             </div>
             {/* <Typewriter childClass="navbar-item" childId="navbar-title" text="WEBSITE" msDelay={100} /> */}
-            <h1 className="navbar-mid-item" id="navbar-title">WEBSITE</h1>
+            <h1 className="navbar-mid-item" id="navbar-title">PHILIP</h1>
             <div className="navbar-item">
                 <div id="navbar-link-wrapper">
                     {navBarButtons}
--- src/componenets/pages/Home.tsx
+++ src/componenets/pages/Home.tsx
@@ -1,10 +1,23 @@
 import React from "react";
+import './Home.css'
 
 export default function Home(): JSX.Element {
     return (
         <div>
-            <p>
-            </p>
+            <section id="introduction-section">
+                <div id="top-line" className="diagonal thin-line full-width line-bg" />
+                <div id="middle-line" className="diagonal mega-thick-line full-width line-grad">
+                    <div id="middle-wrapper">
+                        <div id="middle-title-wrapper">
+                            <div className="side-line"></div><h1 className="big-title">ABOUT ME</h1><div className="side-line"></div>
+                        </div>
+                        <p className="sig-paragraph centred-paragraph">Hello! My name is Philip and I extremely enjoy programming! <br/> I have been developing since 2018
+                        and have developed almost daily since. <br/> Over the years I have worked with quite a few programming languages.
+                        </p>
+                    </div>
+                </div>
+                <div id="bottom-line" className="diagonal thin-line full-width line-bg" />
+            </section>
         </div>
     );
 }
